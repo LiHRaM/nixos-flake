dbafd75e210a3f67b7bd2dace81328c2ed87d078
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2022-01-23 13:43:53 +0100

    feat: add podman and fonts

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,15 @@
       ./modules/default.nix
       ./packages.nix
       ./users.nix
+      ./fonts.nix
     ];
+
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+    };
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
