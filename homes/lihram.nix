7f9c47d5c31797c89962e737e46745f7921a44f1
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2022-01-23 23:22:09 +0100

    feat: remove neovim from home config

--- homes/lihram.nix
+++ homes/lihram.nix
@@ -1,36 +1,33 @@
 { config, pkgs, ... }:
 {
-	home.username = "lihram";
-	home.homeDirectory = "/home/lihram";
+    home.username = "lihram";
+    home.homeDirectory = "/home/lihram";
 
-	home.stateVersion = "22.05";
-	
-	programs.home-manager.enable = true;
-	programs.neovim.enable = true;
-	programs.neovim.viAlias = true;
-	programs.neovim.vimAlias = true;
+    home.stateVersion = "22.05";
 
-	home.packages = [
-          pkgs.gnome3.gnome-tweaks
-	];
+    programs.home-manager.enable = true;
 
-	gtk.enable = true;
-	gtk.theme.name = "Adwaita-dark";
+    home.packages = [
+        pkgs.gnome3.gnome-tweaks
+    ];
 
-	dconf.settings = {
-		"org/gnome/desktop/interface" = {
-			document-font-name = "sans 10";
-			font-name = "sans 10";
-			monospace-font-name = "monospace 10";
-		};
+    gtk.enable = true;
+    gtk.theme.name = "Adwaita-dark";
 
-		"org/gnome/desktop/peripherals/mouse" = {
-			natural-scroll = false;
-		};
+    dconf.settings = {
+        "org/gnome/desktop/interface" = {
+            document-font-name = "sans 10";
+            font-name = "sans 10";
+            monospace-font-name = "monospace 10";
+        };
 
-		"org/gnome/desktop/peripherals/touchpad" = {
-			natural-scroll = true;
-			tap-to-click = true;
-		};
-	};
+        "org/gnome/desktop/peripherals/mouse" = {
+            natural-scroll = false;
+        };
+
+        "org/gnome/desktop/peripherals/touchpad" = {
+            natural-scroll = true;
+            tap-to-click = true;
+        };
+    };
 }
